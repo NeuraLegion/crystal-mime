876c53fc306969aaf0472a293e8d3c33b995cd09
Jonathan Siegel <jonathan@m1max-16.lan>
2023-02-24 22:16:58 +0900

    Initial import

---
+++ README.md
@@ -0,0 +1,39 @@
+# crystal-mime
+
+TODO: Write a description here
+
+## Installation
+
+1. Add the dependency to your `shard.yml`:
+
+   ```yaml
+   dependencies:
+     crystal-mime:
+       github: your-github-user/crystal-mime
+   ```
+
+2. Run `shards install`
+
+## Usage
+
+```crystal
+require "crystal-mime"
+```
+
+TODO: Write usage instructions here
+
+## Development
+
+TODO: Write development instructions here
+
+## Contributing
+
+1. Fork it (<https://github.com/your-github-user/crystal-mime/fork>)
+2. Create your feature branch (`git checkout -b my-new-feature`)
+3. Commit your changes (`git commit -am 'Add some feature'`)
+4. Push to the branch (`git push origin my-new-feature`)
+5. Create a new Pull Request
+
+## Contributors
+
+- [Jonathan Siegel](https://github.com/your-github-user) - creator and maintainer
---
+++ src/crystal-mime.cr
@@ -0,0 +1,79 @@
+require "mime/multipart"
+require "time"
+
+# TODO: Write documentation for `Crystal::Mime`
+module Crystal::Mime
+  VERSION = "0.1.0"
+
+  struct Email
+    property from
+    property to
+    property subject
+    property datetime
+    def initialize(@from : String, @to : String, @subject : String, @datetime : Time)
+    end
+  end
+
+  def self.pack_h_star(str_array)
+    String.build do |io|
+      str_array.each do |number|
+        if number.to_i(16) > 256
+          puts("** Ignoring **")
+          puts("Num: #{number}")
+          puts("Num.to_i: #{number.to_i(16)}")
+          puts("Num.to_i.chr: #{number.to_i(16).chr}")
+        else
+          io.write_byte number.to_i(16).to_u8
+        end
+      end
+    end
+  end
+
+  def self.unescape(string)
+    str=string.tr("+", " ").gsub(/((?:%[0-9a-fA-F]{2})+)/) do |m|
+      # [m.delete("%")].pack("H*")
+      # puts(m.split("%").compact)
+      pack_h_star(m.split("%", remove_empty: true))
+    end # .force_encoding(encoding)
+    # str.valid_encoding? ? str : str.force_encoding(string.encoding)
+  end
+
+  # Mail looks like:
+  # Content-Type=multipart%2Fmixed%3B+boundary%3D%22------------020601070403020003080006%22&Date=Fri%2...
+  def self.parse_raw(escaped_mime_data)
+    unescaped_mime_data = HTML.unescape(escaped_mime_data)
+    # puts unescaped_mime_data.split("\n").first.split("&").first
+    data    = Hash(String, String).new
+    unescaped_mime_data.split("&").each do |p|
+      k,v = p.split("=")
+      data[k] = unescape(v)
+    end
+    boundary  = "#{MIME::Multipart.parse_boundary(data["Content-Type"])}"
+
+    # Manual parse:
+    parts = Array(String).new
+    buf   = Array(String).new
+    data["body-mime"].split("\n") do |line|
+      if(line == "--#{boundary}")
+        parts << buf.join("\n")
+        buf = Array(String).new
+      elsif(line == "--#{boundary}--") # Terminal boundary
+        parts << buf.join("\n") unless buf.empty?
+        buf = Array(String).new # But really should be done
+      else
+        buf << line
+      end
+    end
+    non_mime = parts.shift # https://en.wikipedia.org/wiki/MIME#Multipart_messages
+    return { headers: data, non_mime: non_mime, mime: parts }
+  end
+
+  def self.mail_object_from_raw(raw_mime_data)
+    parsed = parse_raw(raw_mime_data)
+    Email.new(from:     parsed[:headers]["From"],
+              to:       parsed[:headers]["To"],    
+              subject:  parsed[:headers]["Subject"],    
+              datetime: Time::Format::RFC_2822.parse(parsed[:headers]["Date"]))
+  end
+
+end
